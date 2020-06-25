3290fa7f1c7ee8a5883d7574dbac8f02bf2be1e5
Mandeep Singh <techiesoulsin@gmail.com>
2020-06-25 21:13:50 +0800

    added condition for skipping login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { get,set, remove } from './storage.service';
+import { Player } from './player';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -13,15 +16,25 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router: Router
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.show();
+      this.platform.ready().then(() => {
+        this.statusBar.styleDefault();
+        this.splashScreen.show();
+
+        
+        get("PlayerUser").then((response:Player) => {
+          if (response.id) {
+              this.router.navigate(['app/tabs/id-card']);
+          } else {
+            this.router.navigate(['/']);
+          }
+      });
     });
   }
 }
