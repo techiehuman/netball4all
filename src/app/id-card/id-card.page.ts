75ee96d77f6edefc0a76dbf0d547bfb70a61b43d
techiehuman <mandeepsingh@mandeeps-MacBook-Pro.local>
2020-08-14 10:49:06 +0530

    updated for functions

--- src/app/id-card/id-card.page.ts
+++ src/app/id-card/id-card.page.ts
@@ -5,6 +5,13 @@ import { States } from '../states';
 import { Season } from '../season';
 import { get,set, remove } from '../storage.service';
 
+import {
+  Plugins,
+  PushNotification,
+  PushNotificationToken,
+  PushNotificationActionPerformed } from '@capacitor/core';
+  const { PushNotifications } = Plugins;
+
 
 @Component({
   selector: 'app-id-card',
@@ -28,6 +35,49 @@ export class IdCardPage implements OnInit {
    }
 
    ngOnInit() {
+
+    console.log('Initializing HomePage');
+
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting
+    PushNotifications.requestPermission().then( result => {
+      if (result.granted) {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+      }
+    });
+
+    // On success, we should be able to receive notifications
+    PushNotifications.addListener('registration',
+      (token: PushNotificationToken) => {
+        alert('Push registration success, token: ' + token.value);
+        console.log('Push registration success, token: ' + token.value)
+      }
+    );
+
+    // Some issue with our setup and push will not work
+    PushNotifications.addListener('registrationError',
+      (error: any) => {
+        alert('Error on registration: ' + JSON.stringify(error));
+      }
+    );
+
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener('pushNotificationReceived',
+      (notification: PushNotification) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      }
+    );
+
+    // Method called when tapping on a notification
+    PushNotifications.addListener('pushNotificationActionPerformed',
+      (notification: PushNotificationActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      }
+    );
 
   }
   
